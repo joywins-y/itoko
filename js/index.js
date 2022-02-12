f899c0529fa4d133f2f6116715414049279bec20
windy <1519105541@qq.com>
2022-02-12 19:06:16 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,43 @@
+function onTel() {
+  const input = document.createElement("input");
+  input.setAttribute("readonly", "readonly");
+  input.setAttribute("value", "628984233666");
+  document.body.appendChild(input);
+  input.select();
+  input.setSelectionRange(0, 9999);
+  const copy = document.execCommand("Copy");
+  if (copy) {
+    document.execCommand("Copy");
+  }
+  document.body.removeChild(input);
+}
+
+function onGoolePlay() {
+  window.open(
+    "https://play.google.com/store/apps/details?id=com.ian.grosiraja"
+  );
+}
+
+function onAppStore() {
+  window.open("https://apps.apple.com/cn/app/grosiraja/id1527875183");
+}
+
+function onYouTube() {
+  window.open("https://www.youtube.com/channel/UCRC-awYibWgFSBgAUtQ4Arw");
+}
+
+function onFacebook() {
+  window.open("https://www.facebook.com/Grosiraja-ID-110457654216508");
+}
+
+function onIns() {
+  window.open("https://www.instagram.com/grosiraja_official/");
+}
+
+function onTwitter() {
+  window.open("https://twitter.com/GrosirajaID");
+}
+
+function onDouyin() {
+  window.open("https://vt.tiktok.com/ZSqtNua5/");
+}
